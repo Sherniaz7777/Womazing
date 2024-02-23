25ff7a6aa4c7e6245bd062ff7f67918d54ee4003
Sherniaz7777 <sherniazibragimov10@gmail.com>
2024-02-23 11:26:27 +0600

    create react

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./components/header/Header";
 // import Section1 from './components/section1/Section1'
@@ -13,8 +13,35 @@ import Contacts from "./pages/Contacts";
 import Cart from "./pages/Cart";
 import Detail from "./pages/Detail";
 import { Route, Routes } from "react-router-dom";
+import axios from "axios";
 
 const App = () => {
+
+  
+  
+let  Cards =[]
+
+async function addCard(id) {
+  const { data } = await axios.get(`https://65ce2c1fc715428e8b401f4e.mockapi.io/3/name/${id}`);
+   JSON.parse(localStorage.getItem('cards')) || [];
+
+
+  const existingCard = Cards.find(card => card.id === data.id);
+
+  if (!existingCard) {
+
+      Cards.push(data);
+
+
+
+      localStorage.setItem('cards', JSON.stringify(Cards));
+  }
+
+  console.log(Cards); 
+}
+
+
+
   return (
     <div>
       <Header />
@@ -23,8 +50,8 @@ const App = () => {
         <Route path="/shop" element={<Shop />}></Route>
         <Route path="/about" element={<About />} />
         <Route path="/contacts" element={<Contacts />}></Route>
-        <Route path="/cart" element={<Cart />}></Route>
-        <Route path="/detail/:ProducId" element={<Detail />}></Route>
+        <Route path="/cart" element={<Cart CardBasket={Cards}/>}></Route>
+        <Route path="/detail/:ProducId" element={<Detail addCard={addCard}/>}></Route>
       </Routes>
       <Footer />
     </div>
--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
 
 const Contacts = () => {
   return (
-    <div>Contacts</div>
-  )
-}
+    <div>
+      <div className="Container">
+        <h1 className="Shop-h1">Контакты</h1>
+        <div className="Karta">
+          <p>Карта с любой точкой</p>
+        </div>
 
-export default Contacts
+        <div className="map-contact">
+          <div>
+            <p>Телефон</p>
+            <h5>+7 (495) 823-54-12</h5>
+          </div>
+          <div>
+            <p>E-mail</p>
+            <h5>info@sitename.com</h5>
+          </div>
+          <div>
+            <p>Адрес</p>
+            <h5>г. Москва, 3-я улица Строителей, 25</h5>
+          </div>
+        </div>
+
+        <div className="inputs-contact">
+          <h1>Напишите нам</h1>
+          <div className="inputs-contact-in">
+
+          <input id='inputs-contact-input' type="text" placeholder="Имя"/>
+          <br />
+          <input id="inputs-contact-input2" type="email" placeholder="E-mail"/>
+          <br />
+          <input id="inputs-contact-input3" type="text" placeholder="Телефон"/>
+          <br />
+          <textarea  id="" placeholder="Сообщение"></textarea>
+          <br />
+          </div>
+          <button>Отправить</button>
+        </div>
+        <div className="sobshenia">
+          <p>Сообщение успешно отправлено</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contacts;
